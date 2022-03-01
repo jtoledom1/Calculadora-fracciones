d03ded2fdf5c88a6748395158b616f671c67d210
José Toledo Muñoz <JtoledoM1@outlook.com>
2022-03-01 14:40:41 -0600

    Crear solución crear proyecto

---
+++ Quebrado1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quebrado1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
