27b71aad446d8d1ec0678f9549c260002c929ee5
José Toledo Muñoz <JtoledoM1@outlook.com>
2022-03-01 14:52:08 -0600

    Crear clase

---
+++ Quebrado1/Fracción.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quebrado1
+{
+    internal class Fracción
+    {
+        #region propiedades
+        public int Numerador { get; set; }
+        public int Denominador { get; set; }
+        #endregion
+        #region Métodos
+        public Fracción()
+        {
+            this.Numerador = 0;
+            this.Denominador = 1;
+        }
+        public Fracción(int numerador,int denominador)
+        {
+            this.Numerador = numerador;
+            this.Denominador = numerador;
+        }
+        #endregion
+
+    }
+}
