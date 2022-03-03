6f6997ee89bd6d02a46d7937730ac4edf9f68699
José Toledo Muñoz <JtoledoM1@outlook.com>
2022-03-03 13:28:48 -0600

    Se agregó NombreOperador

--- Quebrado1/Fracción.cs
+++ Quebrado1/Fracción.cs
@@ -9,17 +9,27 @@ namespace Quebrado1
     internal class Fracción
     {
         #region propiedades
+        public string NombreOperando { get; set; }
         public int Numerador { get; set; }
         public int Denominador { get; set; }
         #endregion
+
         #region Métodos
         public Fracción()
         {
+            this.NombreOperando = "A";
             this.Numerador = 0;
             this.Denominador = 1;
         }
         public Fracción(int numerador,int denominador)
         {
+            this.NombreOperando = "A";
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+        public Fracción(int numerador, int denominador, string nombreOperando)
+        {
+            this.NombreOperando = nombreOperando;
             this.Numerador = numerador;
             this.Denominador = denominador;
         }
